be184b56306613c406541378fde341c22fe7fbd6
Kishore-Karic <kirukishore89@gmail.com>
2023-03-17 15:23:33 +0530

    Android Build
    Building for Android and export as Apk

---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour
+{
+    [SerializeField] private Button quitButton, okButton, cancelButton;
+    [SerializeField] private GameObject buttonLayer, quitLayer;
+    private void Awake()
+    {
+        quitButton.onClick.AddListener(Quit);
+        okButton.onClick.AddListener(QuitOk);
+        cancelButton.onClick.AddListener(QuitCancel);
+    }
+
+    private void Quit()
+    {
+        buttonLayer.SetActive(false);
+        quitLayer.SetActive(true);
+    }
+
+    private void QuitOk()
+    {
+        Application.Quit();
+    }
+
+    private void QuitCancel()
+    {
+        buttonLayer.SetActive(true);
+        quitLayer.SetActive(false);
+    }
+}
